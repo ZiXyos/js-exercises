acba328333342b3053066fa74d5cca9e79999c98
ZiXyos <killian.moudery@epitech.eu>
2020-06-04 19:16:13 +0200

    G, and I Done

--- week-1/.archive/G-numbers/exercise.js
+++ week-1/.archive/G-numbers/exercise.js
@@ -1,1 +1,13 @@
 // Start by creating a variables `numberOfStudents` and `numberOfMentors`
+
+function classeroom(numberOfStudents, numberOfMentors) {
+
+    var studentStr = ("Number of students: " + numberOfStudents + '\n');
+    var mentorsStr = ("Number of mentors: " + numberOfMentors + '\n');
+
+    let classes = numberOfMentors + numberOfStudents;
+    const all = ("Total numnber of students and mentors: " + classes);
+    console.log(studentStr + mentorsStr + all);
+}
+
+classeroom(15, 8);
--- week-1/.archive/I-floats/exercise.js
+++ week-1/.archive/I-floats/exercise.js
@@ -1,2 +1,13 @@
 var numberOfStudents = 15;
 var numberOfMentors = 8;
+
+function aight_student(stu, ment) {
+
+    let classes = ment + stu;
+    let percentStudent = (100 * stu / classes);
+    let percentMentors = (100 * ment / classes);
+    console.log("Percentage students: " + Math.round(percentStudent) + '%');
+    console.log("Percentage students: " + Math.round(percentMentors) + '%');
+}
+
+aight_student(numberOfStudents, numberOfMentors);
